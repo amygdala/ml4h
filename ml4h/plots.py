e5414b8d7b06549e3a32d17564a5959c706320ab
Samwell Freeman <lucidtronix@gmail.com>
2020-12-05 09:08:29 -0500

    bump

--- ml4h/plots.py
+++ ml4h/plots.py
@@ -2022,8 +2022,12 @@ def plot_saliency_maps(data: np.ndarray, gradients: np.ndarray, paths: List, pre
     if len(data.shape) == 4:
         _plot_3d_tensor_slices_as_rgb(_scale_tensor_inplace(mean_saliency), f'{prefix}_batch_mean_saliency{IMAGE_EXT}', cols, rows)
         _, axes = plt.subplots(1, 1, figsize=(cols * 4, rows * 4))
-        mean_slice = np.mean(mean_saliency, axis=-2)
-        axes.imshow(mean_slice)
+        mean_batch_slice = np.mean(data, axis=(0, -1))
+        mean_batch_saliency = np.mean(mean_saliency, axis=-2)
+        mean_batch_saliency[..., 0] += mean_batch_slice
+        mean_batch_saliency[..., 1] += mean_batch_slice
+        mean_batch_saliency[..., 2] += mean_batch_slice
+        axes.imshow(mean_batch_saliency)
         plt.savefig(f'{prefix}_global_mean_saliency{IMAGE_EXT}')
     logging.info(f"Saved saliency maps at:{prefix}")
 
