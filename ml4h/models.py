2c0347689b2abc66f745d333665ccf0824c0a7e4
Samwell Freeman <lucidtronix@gmail.com>
2020-12-23 07:00:52 -0500

    fixes

--- ml4h/models.py
+++ ml4h/models.py
@@ -1691,7 +1691,7 @@ def _make_multimodal_multitask_model_block(
     decoder_outputs = []
     for tm, decoder_block in decoder_block_functions.items():  # TODO this needs to be a topological sorted according to parents hierarchy
         reconstruction = decoder_block(latent_inputs, intermediates)
-        decoders[tm] = Model(latent_inputs, reconstruction, name=tm.output_name())
+        decoders[tm] = Model(latent_inputs, reconstruction)
         decoder_outputs.append(decoders[tm](multimodal_activation))
 
     return Model(inputs=list(inputs.values()), outputs=decoder_outputs), encoders, decoders
