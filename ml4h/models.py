cc18e3966df73af216b08637010c09770be5867e
Samwell Freeman <lucidtronix@gmail.com>
2020-12-24 05:29:51 -0500

    fixes

--- ml4h/models.py
+++ ml4h/models.py
@@ -1208,6 +1208,7 @@ class GlobalAveragePoolBlock:
             dense_regularize_rate: float,
             **kwargs,
     ):
+        self.dense_layers = dense_layers
         self.fully_connected = FullyConnectedBlock(
             widths=dense_layers,
             activation=activation,
@@ -1218,11 +1219,22 @@ class GlobalAveragePoolBlock:
         ) if dense_layers else None
 
     def __call__(self, x: Tensor, intermediates: Dict[TensorMap, List[Tensor]]) -> Tensor:
-        y = [Flatten()(x[-1]) for tm, x in intermediates.items() if len(x[-1].shape) == 2]  # Flat tensors
-        y += [global_average_pool(x[-1]) for tm, x in intermediates.items() if len(x[-1].shape) > 2]  # Structured tensors
+        y = [Flatten()(x[-1]) for tm, x in intermediates.items() if tm.axes() == 1]  # Flat tensors
+        y += [global_average_pool(x[-len(self.dense_layers)]) for tm, x in intermediates.items() if tm.axes() > 1]  # Structured tensors
         y = concatenate(y) if len(y) > 1 else y[0]
         y = self.fully_connected(y) if self.fully_connected else y
         return y
+
+
+class AverageBlock:
+    """
+    Average the last tensors in intermediates dictionary
+    """
+    def __init__(self, **kwargs):
+        pass
+
+    def __call__(self, x: Tensor, intermediates: Dict[TensorMap, List[Tensor]]) -> Tensor:
+        return Average()([Flatten()(x[-1]) for _, x in intermediates.items()])
 
 
 class PairLossBlock:
