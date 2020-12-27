374fbc05e74867dbb62d6d39e6daf89dc1f1c418
Samwell Freeman <lucidtronix@gmail.com>
2020-12-27 08:25:43 -0500

    fixes

--- ml4h/models.py
+++ ml4h/models.py
@@ -1833,7 +1833,7 @@ def _make_multimodal_multitask_model_block(
     decoders: Dict[TensorMap, Model] = {}
     decoder_outputs = []
     for tm, decoder_block in decoder_block_functions.items():  # TODO this needs to be a topological sorted according to parents hierarchy
-        # Cannot save isolated decoders for UNETs because they require skip connection as inputs as well as latent space
+        # Do not save isolated decoders for UNETs because they require skip connection inputs as well as latent space
         if len([tm_in for tm_in, _ in encoder_block_functions.items() if tm in u_connect[tm_in]]) > 0:
             reconstruction = decoder_block(multimodal_activation, intermediates)
             decoder_outputs.append(reconstruction)
