99032fd956a7a828c698be9b3e60054141aeb2a7
Samwell Freeman <lucidtronix@gmail.com>
2021-03-15 14:08:52 -0400

    add tensormaps

--- ml4h/tensormap/ukb/mri.py
+++ ml4h/tensormap/ukb/mri.py
@@ -1650,19 +1650,19 @@ myocardium_mask_systole_as_random_time = TensorMap(
                                           LAX_4CH_MYOCARDIUM_LABELS,
                                           15),
 )
-myocardium_mask_i5_as_random_time = TensorMap(
+myocardium_mask_i7_as_random_time = TensorMap(
     'myocardium_mask_lax_4ch_random_time', Interpretation.CONTINUOUS, shape=(120, 180, 1), path_prefix='ukb_cardiac_mri',
     tensor_from_file=_heart_mask_instance('cine_segmented_lax_4ch/2/',
                                           'cine_segmented_lax_4ch_annotated_',
                                           LAX_4CH_MYOCARDIUM_LABELS,
-                                          5),
+                                          7),
 )
 myocardium_mask_systole_guess = TensorMap(
     'myocardium_mask_systole_guess', Interpretation.CONTINUOUS, shape=(120, 180, 1), path_prefix='ukb_cardiac_mri',
     tensor_from_file=_heart_mask_instance('cine_segmented_lax_4ch/2/',
                                           'cine_segmented_lax_4ch_annotated_',
                                           LAX_4CH_MYOCARDIUM_LABELS,
-                                          15),
+                                          8),
     normalization=ZeroMeanStd1(), cacheable=False,
 )
 
