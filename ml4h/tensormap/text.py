651b9ea94dfac04fc253df5d551d856ec5f94a94
Samwell Freeman <lucidtronix@gmail.com>
2021-03-17 10:59:40 -0400

    fix

--- ml4h/tensormap/text.py
+++ ml4h/tensormap/text.py
@@ -1,8 +1,13 @@
+import os
 import re
 import logging
 from typing import Dict, Tuple, Callable
 
+import h5py
 import numpy as np
+
+from ml4h.defines import TENSOR_EXT
+from ml4h.tensormap.general import get_tensor_at_first_date
 
 
 def token_dictionary_and_text_from_file(
@@ -25,31 +30,41 @@ def token_dictionary_and_text_from_file(
     return ''.join(texts), char2index
 
 
+def token_dictionary_from_hd5_key(
+        tensors: str,
+        path_prefix: str,
+        name: str,
+) -> Dict[str, int]:
+    characters = set()
+    for tp in os.listdir(tensors):
+        if os.path.splitext(tp)[-1].lower() != TENSOR_EXT:
+            continue
+        with h5py.File(tensors + tp, 'r') as hd5:
+            if name in hd5[path_prefix]:
+                characters.update(np.unique(get_tensor_at_first_date(hd5, path_prefix, name)))
+
+    logging.info(f'Total characters from HD5 Tensor {path_prefix} and name {name}: {len(characters)}')
+    char2index = dict((c, i) for i, c in enumerate(sorted(list(characters))))
+    logging.info(f'char2index:\n {char2index} \n')
+    return char2index
+
+
 def random_text_window_tensor(
     text: str,
     window_size: int,
-    one_hot: bool = True,
 ) -> Callable:
     def text_from_file(tm, _, dependents={}):
         tensor = np.zeros(tm.shape, dtype=np.float32)
         random_index = np.random.randint(window_size, len(text)-window_size)
         for i, c in enumerate(text[random_index:random_index+window_size]):
-            if one_hot:
-                tensor[i, tm.channel_map[c]] = 1.0
-            else:
-                tensor[i] = tm.channel_map[c]
+            tensor[i] = tm.channel_map[c]
         if tm.dependent_map is not None:
             for i, dm in enumerate(tm.dependent_map):
                 start_next_window = random_index+1+i
                 dependents[dm] = np.zeros(dm.shape, dtype=np.float32)
-                if dm.axes() == 1 and one_hot:
-                    dependents[dm][dm.channel_map[text[start_next_window]]] = 1.0
-                elif dm.axes() == 2 or (not one_hot and dm.axes() == 1):
+                if dm.axes() == 1:
                     for j, c in enumerate(text[start_next_window:start_next_window+dm.shape[0]]):
-                        if one_hot:
-                            dependents[dm][j, dm.channel_map[c]] = 1.0
-                        else:
-                            dependents[dm][j] = dm.channel_map[c]
+                        dependents[dm][j] = dm.channel_map[c]
                 else:
                     raise ValueError(f'No method to process dependent map:{dm.name} of shape {dm.shape}.')
                 logging.debug(f'\nInput text: {text[random_index:random_index+window_size]}\n Dependent: {text[start_next_window:start_next_window+dm.shape[0]]}')
@@ -64,3 +79,25 @@ def _preprocess_sentence(sentence, remove_special_chars):
         sentence = re.sub(r"[^a-zA-Z?.!,]+", " ", sentence)
         sentence = sentence.strip()
     return sentence
+
+
+def random_array_window_tensors(
+    window_shape: Tuple[int],
+    shift_axis: int = 0,
+) -> Callable:
+    def window_as_text_from_file(tm, hd5, dependents={}):
+        full_tensor = get_tensor_at_first_date(hd5, tm.path_prefix, tm.name)
+        indexes = [np.random.randint(window_shape[i], edge-window_shape[i]) for i, edge in enumerate(full_tensor.shape)]
+        random_window = tuple(slice(window_shape[i], index) for i, index in enumerate(indexes))
+        next_window1 = tuple(slice(window_shape[i], index + 1 if i == shift_axis else index) for i, index in enumerate(indexes))
+        next_window2 = tuple(slice(window_shape[i], index + 2 if i == shift_axis else index) for i, index in enumerate(indexes))
+        tensor = full_tensor[random_window].flatten()
+        if tm.dependent_map is not None:
+            for dm, window in zip(tm.dependent_map, [next_window1, next_window2]):
+                dependents[dm] = np.zeros(dm.shape, dtype=np.float32)
+                flat = full_tensor[window].flatten()
+                for j, c in enumerate(flat):
+                    dependents[dm][j] = dm.channel_map[c]
+        logging.debug(f'Full shape:{full_tensor.shape} window_shape:{window_shape} random idx:{indexes} tensor shape:{tensor.shape}')
+        return tensor
+    return window_as_text_from_file
