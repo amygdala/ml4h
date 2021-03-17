d6e471a85427a8f77d1d75bafbf679cb509ef1fd
Samwell Freeman <lucidtronix@gmail.com>
2021-03-17 11:55:14 -0400

    fix

--- ml4h/explorations.py
+++ ml4h/explorations.py
@@ -328,12 +328,12 @@ def sample_from_language_model(language_input, next_input, model, test_data, max
             prediction = model.predict(cur_test)
             next_token = index_2_token[_sample_with_heat(_softmax(prediction[0, -1, :]), 0.5)]
             offset = 1
-            full_sentence += next_token
+            full_sentence += str(next_token)
             for k in test_data:
                 if 'next' in k:
                     offset = 0
                 for j in range(test_data[k].shape[1]):
-                    cur_test[k][0, -(j+1)] = language_input.channel_map[full_sentence[-(1+j+offset)]]
+                    cur_test[k][0, -(j+1)] = str(language_input.channel_map[full_sentence[-(1+j+offset)]])
             s = ' '.join([str(index_2_token[index]) for index in cur_test[language_input.input_name()][0]])
             s2 = ' '.join([str(index_2_token[index]) for index in cur_test[next_input.input_name()][0]])
         logging.info(f'Model: {full_sentence}             \n --- {i} --- \n')
