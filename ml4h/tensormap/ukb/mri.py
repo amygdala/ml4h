3f2c3e468c90f4d0f3194081f8aaa0ba3b61d9d0
Samwell Freeman <lucidtronix@gmail.com>
2021-03-23 13:53:34 -0400

    fix

--- ml4h/tensormap/ukb/mri.py
+++ ml4h/tensormap/ukb/mri.py
@@ -1717,7 +1717,7 @@ def _segmented_heart_mask_instances(segmentation_key, labels):
 
 segmented_lax_4ch_heart_center_4d = TensorMap(
     'lax_4ch_heart_center', Interpretation.CONTINUOUS,
-    shape=(96, 96, 50, len(LAX_4CH_HEART_LABELS)),
+    shape=(96, 96, 50, len(MRI_LAX_4CH_SEGMENTED_CHANNEL_MAP)),
     path_prefix='ukb_cardiac_mri', normalization=ZeroMeanStd1(), channel_map=MRI_LAX_4CH_SEGMENTED_CHANNEL_MAP,
     tensor_from_file=_segmented_heart_mask_instances('cine_segmented_lax_4ch_annotated_', LAX_4CH_HEART_LABELS),
 )
