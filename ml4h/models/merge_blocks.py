79bd3ac0b06168d1c2c08ba09c99d0de8562b8b4
Samwell Freeman <lucidtronix@gmail.com>
2021-04-05 14:05:11 -0400

    updates

--- ml4h/models/merge_blocks.py
+++ ml4h/models/merge_blocks.py
@@ -161,9 +161,9 @@ def contrastive_difference(left, right, batch_size=4):
     logging.info(f'tf.shape(logits): {logits} ')
     # symmetric loss function
     logging.info(f'tf.shape(logits): {logits.shape[-1]} ')
-    labels = tf.convert_to_tensor(np.arange(batch_size), dtype=tf.float32)
-    loss_i = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.NONE)(K.transpose(logits), labels)
-    #loss_t = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.SUM)(tf.transpose(logits), labels)
+    labels = tf.convert_to_tensor(np.eye(batch_size), dtype=tf.float32)
+    loss_i = tf.keras.losses.CategoricalCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.SUM)(logits, labels)
+    #loss_t = tf.keras.losses.CategoricalCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.SUM)(tf.transpose(logits), labels)
     #loss = (loss_i + loss_t)/2
     logging.info(f'tf.shape(loss_i): {loss_i} ')
     return loss_i
