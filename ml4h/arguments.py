2ba23dadb674aafb25b3fd3fafbe0625a81dcecd
Samwell Freeman <lucidtronix@gmail.com>
2021-04-05 12:22:34 -0400

    updates

--- ml4h/arguments.py
+++ ml4h/arguments.py
@@ -174,7 +174,7 @@ def parse_args():
         '--pairs', nargs=2, action='append',
         help='TensorMap pairs for paired autoencoder. The pair_loss metric will encourage similar embeddings for each two input TensorMap pairs. Can be provided multiple times.',
     )
-    parser.add_argument('--pair_loss', default='cosine', help='Distance metric between paired embeddings', choices=['euclid', 'cosine'])
+    parser.add_argument('--pair_loss', default='euclid', help='Distance metric between paired embeddings', choices=['euclid', 'cosine', 'contrastive'])
     parser.add_argument('--pair_loss_weight', type=float, default=1.0, help='Weight on the pair loss term relative to other losses')
     parser.add_argument(
         '--max_parameters', default=50000000, type=int,
