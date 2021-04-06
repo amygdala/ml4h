81d904b6bccfd714ef2a2e2a4f09ae50265aa425
Samwell Freeman <lucidtronix@gmail.com>
2021-04-06 07:43:09 -0400

    updates

--- ml4h/models/merge_blocks.py
+++ ml4h/models/merge_blocks.py
@@ -157,7 +157,8 @@ def contrastive_difference(left, right, batch_size=4):
     logging.info(f'tf.shape(I_e): {I_e}  tf.shape(T_e): {T_e}')
     # scaled pairwise cosine similarities [n, n]
    # logits = np.dot(I_e, np.transpose(T_e)) # * np.exp(t)
-    logits = K.clip(K.batch_dot(I_e, K.transpose(T_e)), -1, 1)
+    #logits = K.clip(K.batch_dot(I_e, K.transpose(T_e)), -1, 1)
+    logits = K.clip(K.batch_dot(I_e, T_e), -1, 1)
     logging.info(f'tf.shape(logits): {logits} ')
     # symmetric loss function
     logging.info(f'tf.shape(logits): {logits.shape[-1]} ')
