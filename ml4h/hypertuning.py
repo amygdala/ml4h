7e7529e83fa569664315ed57a65a328e6b29c2b8
Samwell Freeman <lucidtronix@gmail.com>
2021-04-20 11:22:33 -0400

    update

--- ml4h/hypertuning.py
+++ ml4h/hypertuning.py
@@ -61,14 +61,14 @@ def run(args):
 
 def make_model_builder(args):
     def model_builder(hp):
-        num_conv_layers = hp.Int('num_conv_layers', 0, 4)
+        num_conv_layers = hp.Int('num_conv_layers', 1, 4)
         conv_layer_size = hp.Int('conv_layer_size', 16, 128, sampling='log')
         args.__dict__['conv_layers'] = [conv_layer_size] * num_conv_layers
-        num_dense_blocks = hp.Int('num_dense_blocks', 0, 6)
+        num_dense_blocks = hp.Int('num_dense_blocks', 1, 6)
         dense_block_size = hp.Int('dense_block_size', 16, 128, sampling='log')
         args.__dict__['dense_blocks'] = [dense_block_size] * num_dense_blocks
         args.__dict__['block_size'] = hp.Int('block_size', 1, 7)
-        num_dense_layers = hp.Int('num_dense_layers', 0, 4)
+        num_dense_layers = hp.Int('num_dense_layers', 1, 4)
         dense_layer_size = hp.Int('dense_layer_size', 16, 128, sampling='log')
         args.__dict__['dense_layers'] = [dense_layer_size] * num_dense_layers
         model, _, _, _ = block_make_multimodal_multitask_model(**args.__dict__)
