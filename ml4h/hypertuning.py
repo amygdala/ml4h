c5547a3cac3d15828f524211b485f64769dc4f61
Samwell Freeman <lucidtronix@gmail.com>
2021-04-23 09:09:10 -0400

    update

--- ml4h/hypertuning.py
+++ ml4h/hypertuning.py
@@ -62,10 +62,10 @@ def run(args):
 
 def make_model_builder(args):
     def model_builder(hp):
-        num_conv_layers = hp.Int('num_conv_layers', 1, 4)
+        num_conv_layers = hp.Int('num_conv_layers', 0, 4)
         conv_layer_size = hp.Int('conv_layer_size', 16, 128, sampling='log')
         args.__dict__['conv_layers'] = [conv_layer_size] * num_conv_layers
-        num_dense_blocks = hp.Int('num_dense_blocks', 1, 6)
+        num_dense_blocks = hp.Int('num_dense_blocks', 1, 3)
         dense_block_size = hp.Int('dense_block_size', 16, 128, sampling='log')
         args.__dict__['dense_blocks'] = [dense_block_size] * num_dense_blocks
         args.__dict__['block_size'] = hp.Int('block_size', 1, 7)
