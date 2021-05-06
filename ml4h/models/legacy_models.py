9bf047258671fb64b89480258b77eebe86898dbe
Samwell Freeman <lucidtronix@gmail.com>
2021-05-06 10:57:38 -0400

    fix

--- ml4h/models/legacy_models.py
+++ ml4h/models/legacy_models.py
@@ -341,7 +341,7 @@ def make_hidden_layer_model(parent_model: Model, tensor_maps_in: List[TensorMap]
                 parent_inputs = [layer.get_layer(tm.input_name()).input for tm in tensor_maps_in]
                 break
             except ValueError:
-                logging.warning(f'Value error searching for layer: {output_layer_name}')
+                logging.warning(f'Value error searching for layer: {output_layer_name} at layer: {layer.name}')
                 continue
     if not target_layer:
         target_layer = parent_model.get_layer(output_layer_name)
