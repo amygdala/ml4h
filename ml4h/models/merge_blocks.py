66cee24a5628d4b40090e4c1dbaa3e6d84354338
Samwell Freeman <lucidtronix@gmail.com>
2021-05-27 13:52:25 -0400

    simple conv

--- ml4h/models/merge_blocks.py
+++ ml4h/models/merge_blocks.py
@@ -158,16 +158,13 @@ class PairLossBlock(Block):
             return concatenate(y)
         elif self.pair_merge == 'dropout':
             # get random index vector
-            tf_y = tf.convert_to_tensor(y)
-            tf.print(f'y shape {len(y)} tf_y {tf_y.shape}')
-            random_index = np.random.randint(len(y), size=(self.batch_size, intermediates[left][-1].shape[-1]))
+            random_index = np.random.randint(len(y), size=intermediates[left][-1].shape[-1])
             tf.print(f'random index {random_index.shape} random_index {random_index[:4]}')
-            tf_t = tf.transpose(tf_y, perm=[1, 2, 0])
-            tf.print(f' new shape {tf_t.shape}')
-            tf_tnp = tf_t.numpy()
-            out = tf_tnp[random_index]
-            tf.print(f'out shape {out.shape}')
-            return tf.convert_to_tensor(out)
+            dropped_y = [y[idx][:, i] for i, idx in enumerate(random_index)]
+            tf.print(f'y shape {len(dropped_y)} tf_y {dropped_y[:4]}')
+            tf_y = tf.convert_to_tensor(dropped_y)
+            tf.print(f'out shape {tf_y.shape}')
+            return tf_y
 
 
 def contrastive_difference(left: tf.Tensor, right: tf.Tensor, batch_size: int, temperature: tf.Tensor):
