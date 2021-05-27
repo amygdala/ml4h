c85616895cdbe31dcde519a9172c69b41ac78fad
Samwell Freeman <lucidtronix@gmail.com>
2021-05-27 13:14:30 -0400

    simple conv

--- ml4h/models/merge_blocks.py
+++ ml4h/models/merge_blocks.py
@@ -162,9 +162,9 @@ class PairLossBlock(Block):
             tf.print(f'y shape {len(y)} tf_y {tf_y.shape}')
             random_index = np.random.randint(len(y), size=intermediates[left][-1].shape[-1])
             tf.print(f'random index {random_index.shape} random_index {random_index[:4]}')
-            tf_t = tf.transpose(tf_y, perm=[1, 0, 2])
+            tf_t = tf.transpose(tf_y, perm=[1, 2, 0])
             tf.print(f' new shape {tf_t.shape}')
-            out = tf.gather(tf_t, indices=random_index, batch_dims=-1)
+            out = tf.gather(tf_t, indices=random_index, axis = 2, batch_dims=1)
             tf.print(f'out shape {out.shape}')
             return out
 
