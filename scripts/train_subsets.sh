d2b8b7f5e8097d1af6ce2493c5de13eecef4f207
Samuel Friedman <lucidtronix@gmail.com>
2021-07-08 10:02:04 -0400

    subsets

--- scripts/train_subsets.sh
+++ scripts/train_subsets.sh
@@ -13,8 +13,8 @@ do
     --decoder_blocks /home/sam/trained_models/hypertuned_48m_16e_ecg_median_raw_10_autoencoder_256d/decoder_ecg_rest_median_raw_10.h5 \
                      /home/sam/trained_models/hypertuned_32m_8e_lax_4ch_heart_center_autoencoder_256d/decoder_lax_4ch_heart_center.h5 \
     --pairs "$TENSOR_MAPS" --pair_loss contrastive --pair_loss_weight 0.1 --pair_merge dropout \
-    --batch_size 4 --epochs 1 --training_steps 128 --validation_steps 32 --test_steps 1 \
-    --num_workers 4 --patience 36 --tensormap_prefix ml4h.tensormap.ukb \
+    --batch_size 4 --epochs 316 --training_steps 128 --validation_steps 32 --test_steps 1 \
+    --num_workers 4 --patience 48 --tensormap_prefix ml4h.tensormap.ukb \
     --id "drop_fuse_${i%.*}" --output_folder /home/sam/trained_models/ \
     --inspect_model \
     --train_csv "/home/sam/csvs/${i}" \
